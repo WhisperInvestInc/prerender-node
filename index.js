03999b66011cbf6051b79398b176a1c32acdd1af
Zoheb Sait <zoheb.sait@gmail.com>
2015-03-22 18:26:13 -0400

    fix

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var prerender = module.exports = function(req, res, next) {
 // we support _escaped_fragment_ and want to ensure people aren't
 // penalized for cloaking.
 prerender.crawlerUserAgents = [
-   'googlebot',
+   'google',
    'yahoo',
    'bingbot',
   'baiduspider',
